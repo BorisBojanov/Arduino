f63fe0fcba90f687c73ae8ee9402e8882fd9c3eb
BorisBojanov <Boris.bojanov@gmail.com>
2026-02-24 18:00:01 -0700

    Lots O Libraries

---
+++ Favero_Repeater-main/Favero_Repeater-main/README.md
@@ -0,0 +1,85 @@
+# Repeater lights for Favero scoring machines
+
+The Favero Full Arm scoring machines use serial data output that can be interpreted through an Arduino and then used to send data signals for ws2812b LED lights for strip repeater lights. This should work for the FA-01, FA-05, and FA-07. I tested this with a FA-07.
+
+This uses one [esp8266 Wemos D1 mini](https://www.amazon.com/Organizer-ESP8266-Internet-Development-Compatible/dp/B081PX9YFV/ref=sr_1_3?crid=3HISM104Q8NMO&keywords=wemos+d1+mini&qid=1636054524&qsid=133-6299387-8119633&sprefix=wemos%2Caps%2C400&sr=8-3&sres=B081PX9YFV%2CB08C7FYM5T%2CB08QZ2887K%2CB07W8ZQY62%2CB07RBNJLK4%2CB08FZ9YDGS%2CB07BK435ZW%2CB07V84VWSM%2CB07PF3NK12%2CB08H1YRN4M%2CB08FQYZX37%2CB073CQVFLK%2CB07VN328FS%2CB0899N647N%2CB07G9HZ5LM%2CB08MKLRSNH&srpt=SINGLE_BOARD_COMPUTER) per fencing strip, and one LED object for each fencer (left and right). The hardware is assembled as shown in the [basic diagram](/diagrams/Favero_repeater_breadboard.png). This is a basic overview of how it works through a breadboard. I recommend using a protoboard/perfboard/pcb for real applications. [Some people swear online](https://youtu.be/H4pFvqIxkhQ?si=C3Unc96JtROeyP44) to put a resistor between the data pin on the LED strip and the microcontroller, but I found my setup worked fine without it. The [full setup](/diagrams/Favero_repeater_full_setup.png) includes this resistor, but it may not be required for you. The full setup also includes the power injection points I am using to ensure my LEDs are able to draw enough power.
+
+
+<h2>Topology</h2>
+
+<h3>Requirements</h3>
+
+- Two ws2812b RGB LED strips and knowing how many total LEDs in each strip to change line 10 in [Wemos_Favero_Serial_Parser.ino](Wemos_Favero_Serial_Parser/Wemos_Favero_Serial_Parser.ino#L10).  
+- Power source for the RGB LED strips and microcontroller (not covered here)
+- A 100 ohm resistor
+- A favero full arm scoring machine with a rj-11 serial port on the back.
+- A microcontroller: I am using a esp8266 Wemo D1 mini in this example (specifically ESP8266 ESP-12 ESP-12F NodeMcu Mini D1 Module WeMos Lua 4M Bytes WLAN WiFi Internet Development Board)
+- An RJ11 cable to connect from your Favero scoring machine to your microcontroller. An easy option is to use a RJ11 breakout such as [this](https://a.co/d/c3JmIds).
+
+<h3>Microcontroller to Favero</h3>
+
+The section in the [diagram](/diagrams/Favero_repeater_breadboard.png) shows what is required from the microcontroller to the Favero scoring machine.
+
+- Plug one end of an RJ-11 cable into the Favero Full-Arm scoring machine serial port on the back: FA-01, FA-05, or FA-07.
+- Wemo 3.3V pin to Favero RJ-11 pin 3 (R).
+- Wemo RX pin to Favero RJ-11 pin 4 (G).
+- Wemo GND pin to Favero RJ-11 pin 5 (Y).
+    - There must be a 100 Ohm resistor in between the Favero scoring machine and GND (no other devices need the 100 ohm resistor).
+    - The GND pin on the Wemo will also need to be connected to your LED strips and their power source ground as well. A common ground between all the components is required.
+
+<h3>Microcontroller to LEDs</h3>
+
+- Connect the "left fencer" LED strip data to D2 (pin 4) (fencer on the left / FOTL) on the Wemo.
+- Connect the "right fencer" LED strip data to D1 (pin 5) (fencer on the right / FOTR) on the Wemo.
+- The GND pins will need to be connected on the same rail to your power source, LED strip, the scoring machine, and the Wemo's ground pin. A 100 ohm resistor is required from GND to the Favera FA scoring machine RJ-11 pin 3 (no other devices). A common ground between all the components is required.
+- Connect your 5v/12v/24v or whatever power source you have to each LED strip for their power. If you need help figuring out how to power your LEDs, https://quinled.info/ is a fantastic resource. 
+
+
+<h3>Edit  Wemos_Favero_Serial_Parser_Rev_1.ino</h3>
+
+- Edit line 10 of [Wemos_Favero_Serial_Parser.ino](Wemos_Favero_Serial_Parser/Wemos_Favero_Serial_Parser.ino#L10) to the total number of LEDs (applies to both fencers).
+- Edit line 46 of [Wemos_Favero_Serial_Parser.ino](Wemos_Favero_Serial_Parser/Wemos_Favero_Serial_Parser.ino#L46) to the power draw you want. This will have FastLED tell the LED strips to only draw a maximum power draw to prevent your LEDs from going out, and set the amps to an appropriate amount. 
+    - `FastLED.setMaxPowerInVoltsAndMilliamps(5, 2000)` - Where `5` is 5 volts, and `2000` is 2000 milliamps (2 amps). This applies to the total output FastLED will allow to be output.
+
+<h3>Serial stream</h3>
+
+Credit for this chart goes to @Gioee: https://github.com/Gioee/fav3er0-master-emulator  
+
+| Byte | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
+|-|-|-|-|-|-|-|-|-|-|-|
+|Example| 0xFF | 0x06 | 0x12 | 0x56 | 0x02 | 0x14 | 0x0A | 0x00 | 0x38 | 0xC5|
+| Explanation| This Byte identifies the beginning of the string, it has to be always 0xFF | Right score, in this case 6 | Left score, in this case 12 | Seconds, in this case 56 | Minutes, in this case 2 | State of the lamps, case 0b00000000 no lamp is activated, case 0b00111111 every lamp is activated | Number of matches and priorite signals | Always 0x00 | Red and yellow penalty cards | Checksum = Sum of previous Bytes % 256 |
+
+- 6° byte: XXh = Define the state of the lamps (red, green, whites and yellows). Every bit defines the state of a lamp (zero=OFF, 1=ON). Following is the correspondence of the 8 bits: 
+    - Bit D0 = Left white lamp 
+    - Bit D1 = Right white lamp
+    - Bit D2 = RED lamp (left)
+    - Bit D3 = GREEN lamp (right)
+    - Bit D4 = Right yellow lamp
+    - Bit D5 = Left yellow lamp 
+    - Bit D6 = 0 not used 
+    - Bit D7 = 0 not used 
+    - Example: if byte 6° = 14h , we have D2=1 (red light=on) and D4=1 (right yellow light=on)
+    - Credit to /u/Dalboz989 on Reddit: https://www.reddit.com/r/Fencing/comments/cufcku/do_anyone_know_where_to_find_those_lightings_and/ey15ezm/
+
+<h3>Troubleshooting</h3>
+
+<h4>My LEDs are showing the wrong color (sometimes)!</h4>
+
+- The code is set to only send signals for the appropriate color for left (red) and right (green) fencers and white for off-target. If the colors are mismatched such as green and red on the opposite sides, then move your led strip or swap the data pin connections. You can also ensure your LED strips are truly RGB, or GRB and edit that in line 44 and 45 in [Wemos_Favero_Serial_Parser.ino](Wemos_Favero_Serial_Parser/Wemos_Favero_Serial_Parser.ino#L44). If you are getting some other color like a blue or a unexpected white, its most likely your LED configuration that is causing the issue. There is a lot of knowledge that you need to know that comes with LED strips (that I hadn't known before this project), such as the power injection or wire gauge for voltage drop off. The site  https://quinled.info/ is a fantastic resource for determing your LED strip power usage, and how to get the best results out of your LEDs. Be sure to watch some of the videos on their website or Youtube channel.
+
+<h4>Serial monitor says I am getting a lot of wrong checksums?</h4>
+
+- This is caused by noise somewhere. My tests were usually the LED data pin causing the noise. I couldn't figure out how to stop it so I added the checksum verification to the serial parser for the microcontroller. The microcontroller will only accept messages from the scoring machine if the checksum matches to prevent corrupted data. In my testing with the checksum verification, I have never seen corrupted data hit the LED strips and turn on a light when a light has not been turned on. Without the checksum verification, electrical noise will send junk data to the serial port and the microcontroller may read a bit as having a light turned on therefore causing the LED strip to turn on a light when the scoring machine does not have that light turned on.
+
+<h4>There is a small delay in the lights when two fencers don't make a touch at the same exact time, but both lights come on.</h4>
+
+- Your guess is as good as mine as to why this occurs. Could be noise causing the serial message to be thrown out so often it causes a delay. This delay DOES occurr on the official Favero repeater I tested with.
+
+---
+
+If you have any improvements, please open a pull request. I am definitely not doing some stuff the most optimal way and I would greatly appreciate any additions.
+
+
+Credit for the serial parsing came from @BenKohn2004: https://github.com/BenKohn2004/Favero_Overlay  
+Credit for discovering the checksum verification that is used goes to @Gioee: https://github.com/Gioee/fav3er0-master-emulator  
---
+++ Favero_Repeater-main/Favero_Repeater-main/Wemos_Favero_Serial_Parser/Wemos_Favero_Serial_Parser.ino
@@ -0,0 +1,199 @@
+#include <FastLED.h>
+#define MY_NAME "REPEATER"
+
+const unsigned int MAX_MESSAGE_LENGTH = 10;
+const unsigned int MAX_SERIAL_BUFFER_BYTES = 512;
+const char STARTING_BYTE = 255;
+
+const int FOTL_DATA = 4; // D2
+const int FOTR_DATA = 5; // D1
+const int NUM_LEDS = 3; // Number of LEDs in your LED strip/matrix
+unsigned int COUNTER = 0;
+bool MACHINE_OFF = true;
+
+CRGB left[NUM_LEDS]; // FOTL LED array
+CRGB right[NUM_LEDS]; // FOTR LED array
+
+struct __attribute__((packed)) dataPacket {
+  int unsigned Right_Score;
+  int unsigned Left_Score;
+  int unsigned Seconds_Remaining;
+  int unsigned Minutes_Remaining;
+  bool Green_Light;
+  bool Red_Light;
+  bool White_Green_Light;
+  bool White_Red_Light;
+  bool Yellow_Green_Light;
+  bool Yellow_Red_Light;
+  bool Yellow_Card_Green;
+  bool Yellow_Card_Red;
+  bool Red_Card_Green;
+  bool Red_Card_Red;
+  bool Priority_Left;
+  bool Priority_Right;
+};
+
+// Shows if new data is available for display
+bool new_data = false;
+
+// Initializes Message_Position
+unsigned int message_pos = 0;
+
+void setup() {
+
+  FastLED.addLeds<WS2812, FOTL_DATA, GRB>(left, NUM_LEDS); // I initialize the LEDs using GRB, your LED strip may be RGB! Confirm in the datasheet for your LED strip.
+  FastLED.addLeds<WS2812, FOTR_DATA, GRB>(right, NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 16000); // This is 5v, 2amps. Change this to your max output between both strips combined.
+
+  Serial.setRxBufferSize(1024);
+  Serial.begin(2400);  // FA-01, FA-05, & FA-07 serial settings: 2400-N-8-1
+  delay(10);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.print("Initializing...");
+  Serial.println(MY_NAME);
+  Serial.println("Initialized.");
+}
+
+void loop() {
+
+  EVERY_N_MILLISECONDS(200) {
+    wdt_reset();
+  }
+
+  dataPacket packet;
+
+  //  Check to see if anything is available in the serial receive buffer
+  while (Serial.available() > 0) {
+    // All of this serial handling was taken from https://github.com/BenKohn2004/Favero_Overlay
+    // Create a place to hold the incoming message
+
+    static char message[MAX_MESSAGE_LENGTH];
+    static char prev_message[MAX_MESSAGE_LENGTH];
+
+    char inByte = Serial.read(); // Retrieve the first byte from the buffer
+
+    // Message coming in (check if starting character)
+    if (inByte == STARTING_BYTE) {
+      message_pos = 0; // Resets message position
+      message[message_pos] = inByte; // Stores the Byte in the message position
+      message_pos++; // Increments message position
+    } else if (message_pos < (MAX_MESSAGE_LENGTH - 1)) {
+      message[message_pos] = inByte; // Stores the Byte in the message position
+      message_pos++; // Increments message position
+    } else if (message_pos == (MAX_MESSAGE_LENGTH - 1)) {
+      message[message_pos] = inByte; // Add the last 10th byte that is the checksum to the array
+      byte checksum = 0; // Reset the checksum value
+      
+      // I took some help from https://github.com/Gioee/fav3er0-master-emulator to figure out this checksum
+      // https://www.reddit.com/r/Fencing/comments/cufcku/do_anyone_know_where_to_find_those_lightings_and/ey15ezm/
+      for (int i = 0; i < MAX_MESSAGE_LENGTH; i++) { 
+        if (i == 9) { // Stop at the 10th byte (9th index) so we don't include the checksum itself in our calculation
+          checksum = checksum % 256; // CheckSum8 Modulo 256 - https://www.scadacore.com/tools/programming-calculators/online-checksum-calculator/
+        } else {
+            checksum += message[i]; // Add the current byte of the loop to the checksum total
+        }
+      }
+
+      if (message[9] == checksum) { // Only accept messages that match the checksum 10th byte and our calculation - prevents corrupted data
+        COUNTER = 0; // To turn off the lights later.
+        MACHINE_OFF = false; // If we got a verified message from the scoring machine, declare the machine is powered on.
+
+        if (message[1] != prev_message[1] or message[2] != prev_message[2] or message[3] != prev_message[3] or message[4] != prev_message[4] or message[5] != prev_message[5] or message[6] != prev_message[6] or message[8] != prev_message[8]) {
+          // Excudes the internal use byte in position 7 or check sum since we already verified the checksum matches
+
+          Serial.print("The message in HEX is: "); // Print the message we got that matches the checksum therefore is not corrupted, and is also different from the previous message
+          for (int i = 0; i < MAX_MESSAGE_LENGTH; i++) {
+            Serial.print(message[i], HEX);
+            Serial.print(",");
+          }
+          Serial.print("\n");
+          new_data = true; // Sets New Data to True
+
+          // Assigns values from message to packet
+          packet.Green_Light = bitRead(message[5], 3);
+          packet.Red_Light = bitRead(message[5], 2);
+          packet.White_Green_Light = bitRead(message[5], 1);
+          packet.White_Red_Light = bitRead(message[5], 0);
+          packet.Yellow_Green_Light = bitRead(message[5], 4);
+          packet.Yellow_Red_Light = bitRead(message[5], 5);
+
+          // This stuff is not needed for repeaters but I left it in case someone wants to use it later
+          packet.Yellow_Card_Green = bitRead(message[8], 2);
+          packet.Yellow_Card_Red = bitRead(message[8], 3);
+          packet.Red_Card_Green = bitRead(message[8], 0);
+          packet.Red_Card_Red = bitRead(message[8], 1);
+
+          packet.Priority_Right = bitRead(message[6], 2);
+          packet.Priority_Left = bitRead(message[6], 3);
+
+          // Score and Time - has to be converted to an int from hex
+          packet.Right_Score = hex_string_to_int(message[1]);
+          packet.Left_Score = hex_string_to_int(message[2]);
+          packet.Seconds_Remaining = hex_string_to_int(message[3]);
+          packet.Minutes_Remaining = hex_string_to_int(message[4]);
+
+          // Sets Previous Message to Current Message - so we know when a unique message comes in later
+          for (int i = 0; i < MAX_MESSAGE_LENGTH; i++) {
+            prev_message[i] = message[i];
+          }
+
+          // Resets Message Position
+          message_pos = 0;
+
+          // Clears the Serial Buffer if more than Max Buffer Bytes bytes in the buffer
+          if (Serial.available() > MAX_SERIAL_BUFFER_BYTES) {
+            Serial.println("...............Clearing the Serial Buffer...............");
+            while (Serial.available() > 0) {
+              char inByte = Serial.read();
+            }
+          }
+          // Right fencer lights
+          if (packet.Green_Light == 1) {
+            fill_solid(right, NUM_LEDS, CRGB::Green); // On target right
+            Serial.println("FOTR ON TARGET");
+          } else if (packet.White_Green_Light == 1) {
+            fill_solid(right, NUM_LEDS, CRGB::White); // Off target right
+            Serial.println("FOTR OFF TARGET");
+          } else {
+            fill_solid(right, NUM_LEDS, CRGB::Black); // No light on right
+            Serial.println("FOTR NO LIGHT");
+          }
+          // Left fencer lights
+          if (packet.Red_Light == 1) {
+            fill_solid(left, NUM_LEDS, CRGB::Red); // On target left
+            Serial.println("FOTL ON TARGET");
+          } else if (packet.White_Red_Light == 1) {
+            fill_solid(left, NUM_LEDS, CRGB::White); // Off target left
+            Serial.println("FOTL OFF TARGET");
+          } else {
+            fill_solid(left, NUM_LEDS, CRGB::Black); // No light on left
+            Serial.println("FOTL NO LIGHT");
+          }
+          FastLED.show(); // Send the changes to the LEDs
+        }
+      } else {
+      Serial.println("Wrong checksum! Throwing it out."); // If the checksum doesn't match, disregard the message and try again
+      }
+    } else {
+        Serial.println("Unexpected Message Position, Reseting to zero.");
+        message_pos = 0;
+    }
+  }
+  
+  /* The Favero FA-05 stops sending serial for small moments of time while it is on so checking for serial to turn off the lights doesn't work.
+  I feel like there is a better way to do this, but this seems to work so I didn't feel like messing with it more.*/
+  COUNTER++;
+  if (COUNTER > 10000) { // turn off the lights if 10,000 loops have passed and haven't gotten any serial connection.
+    if (MACHINE_OFF == false) { // only turn the lights off when the machine was previously on - prevents the spamming of turning off the lights
+      MACHINE_OFF = true;
+      fill_solid(left, NUM_LEDS, CRGB::Black);
+      fill_solid(right, NUM_LEDS, CRGB::Black);
+      Serial.println("Machine is off");
+      FastLED.show();
+    }
+    COUNTER = 0;
+    MACHINE_OFF = true; // Confirmed the machine is off so stop trying to turn off the lights
+      }
+}
